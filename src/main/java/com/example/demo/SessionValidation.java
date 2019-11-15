ee9476a6fab1641eb748e91660d7f44c6ea6a89d
subhash14580 <subhash.nmkns@gmail.com>
2019-11-15 21:06:50 +0530

    Added required Features

--- src/main/java/com/example/demo/SessionValidation.java
+++ src/main/java/com/example/demo/SessionValidation.java
@@ -1,14 +1,60 @@
 package com.example.demo;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
-@Controller	
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequestMapping(value="/")
 public class SessionValidation {
+	@Autowired
+	SessionDAO sessionDAO;
+	
 	@RequestMapping("/sessions")
-	public SessionPO getSessionPO() {
-		SessionPO sessionPo = new SessionPO();
-		sessionPo.setSessionId("123456");
-		return sessionPo;
+	@ResponseBody
+	public List<SessionPO> getSessionPO() {
+		return sessionDAO.findAll();
+	}
+	
+	@PostMapping(value = "/addSession",produces = "application/json", consumes="application/json")
+	@ResponseBody
+	public Map<String,String>  saveSessionPO(@RequestBody  SessionPO session) {
+		System.out.println(session.toString());
+		Map<String,String> map = new HashMap<>();
+		
+		try {
+		 sessionDAO.save(session);
+		}catch(Exception e) {
+			e.printStackTrace();
+			map.put("STATUS", "FAILURE");
+			return map;
+		}
+		map.put("STATUS", "SUCCESS");
+		return map;
+	}
+	
+	@RequestMapping(value ="/isValid")
+	public String isValidSession(@RequestParam Integer userId , @RequestParam String sessionId) {
+		System.out.println("userId--"+userId + " SessionId"+sessionId);
+		if(userId != null) {
+			SessionPO sessionPO = sessionDAO.findById(userId).get();
+			if(sessionPO.getSessionId().equals(sessionId)) {
+				return "SUCCESS";
+			}
+			
+		}
+		return "FAILURE";
 	}
 }
